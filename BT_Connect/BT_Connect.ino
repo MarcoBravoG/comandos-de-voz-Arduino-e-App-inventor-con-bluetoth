654a1b0012e090bc3c37d38f817de73d7ad22f57
ELECTROM@RK <marco.fernando.bravo@gmail.com>
2022-04-06 13:22:47 -0500

    Add files via upload

---
+++ BT_Connect/BT_Connect.ino
@@ -0,0 +1,46 @@
+/*
+Arduino Due + HC-06 (Bluetooth) -echo bluetooth data
+
+Serial (Tx/Rx) communicate to PC via USB
+Serial3 (Tx3/Rx3) connect to HC-06
+HC-06 Rx - Due Tx3
+HC-06 Tx - Due Rx3
+HC-06 GND - Due GND
+HC-06 VCC - Due 3.3V
+
+*/
+#define HC06 Serial3
+
+void setup()
+{
+  delay(1000);
+  Serial.begin(9600);
+  HC06.begin(9600);
+   pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  
+  Serial.write("\nTest Start\n");
+}
+
+void loop()
+{
+  while(HC06.available())
+  {
+    char data = HC06.read();
+    Serial.write(data);
+    HC06.write(data);
+    if (data =='A'){
+      
+      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  
+      }
+
+          if (data =='B'){
+      
+      digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  
+      }
+    
+    
+  }
+}
